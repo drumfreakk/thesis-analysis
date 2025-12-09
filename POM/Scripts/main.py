4aab2c4b5aea6be65235c1b8ce695ed447894591
Kieran H <k.a.houtgraaf@students.uu.nl>
2025-12-09 16:17:25 +0100

    avg density

--- POM/Scripts/main.py
+++ POM/Scripts/main.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python
 
-import numpy as np
+import sys
+sys.path.insert(1, '../../')
+import standard
 
 import os
 
-import matplotlib  as mpl 
+import numpy as np
+
 import matplotlib.pyplot as plt 
 import matplotlib.patches as mpatches
 
@@ -15,127 +18,164 @@ from skimage.measure import label, regionprops
 
 import aux_functions
 
+def make_figure(name, magnification, save=False):
+	datapath = '.'
+	
+	# Assume the pixel spacing is isotropic
+	# The spacing is in um/px
+	match magnification:
+		case '5x':
+			spacing = 1/2.03
+			length_scalebar_um = 50
+		case '10x':
+			spacing = 1/4.09
+			length_scalebar_um = 50
+		case '20x':
+			spacing = 1/8.17
+			length_scalebar_um = 50
+		case '40x':
+			spacing = 1/16.56
+			length_scalebar_um = 20
+		case _:
+			spacing = 1
+			length_scalebar_um = 0
+			print("WARNING: No magnification found, guessing pixel size")
+		
+	### Load and threshold the picture
+	
+	img_original = ski.io.imread(os.path.join(datapath, name))
+	img_gray = ski.color.rgb2gray(ski.util.img_as_int(img_original))
+	
+	threshold = ski.filters.threshold_yen(img_gray)
+	img_thresh = img_gray >= threshold
+	
+	
+	
+	### Identify the droplets
+	
+	# Label elements on the picture
+	black = ski.util.dtype_limits(img_thresh)[0]
+	label_image = label(img_thresh, background=black)
+	
+	fig, axs = plt.subplots(ncols=1, nrows=2, figsize=(12, 12), height_ratios=[3,1])
+	axs[0].imshow(img_original)
+	
+	length_scalebar = length_scalebar_um/spacing
+	scalebar = mpatches.Rectangle((50, img_thresh.shape[0] - 100),\
+								  length_scalebar, 50, color='white')
+	axs[0].add_patch(scalebar)
+	axs[0].text(50, img_thresh.shape[0] - 120,\
+				"\\boldmath$" + str(length_scalebar_um) + " \\mathrm{\\mu m}$",\
+				color='white', size='large')
+	
+	
+	round_droplets = []
+	elongated_droplets = []
+	
+	# Analyze each droplet
+	for region in regionprops(label_image, intensity_image=img_gray, spacing=(spacing,spacing)):
+		# Skip regions smaller than 50 square pixels
+		if region.area < 50 * spacing**2:
+			continue
+		
+		# Skip regions smaller than 2 um^2
+		if region.area < 2:
+			continue
+	
+		# Skip regions close to the edge of the image
+		if region.bbox[0] < 10 or\
+		   region.bbox[1] < 10 or\
+		   region.bbox[2] > img_thresh.shape[0] - 10 or\
+		   region.bbox[3] > img_thresh.shape[1] - 10:
+		   continue
+	
+		if region.eccentricity > 0.7:
+			elongated_droplets.append(region)
+	#		print(round(region.area_filled,2), '\t', round(region.intensity_mean,2))
+		else:
+			round_droplets.append(region)
+	#		print('\t', round(region.equivalent_diameter_area,3))
+	
+	# Doesn't actually do anything at the moment
+	elongated_droplets, new_round = aux_functions.merge_nearby_regions(elongated_droplets)
+	round_droplets += new_round
+	
+	### Display the droplet outlines
+	
+	for region in round_droplets:
+		minr, minc, maxr, maxc = region.bbox
+		color = 'red'
+		
+		circ = mpatches.Circle((region.centroid[1]/spacing, region.centroid[0]/spacing),\
+								radius=region.equivalent_diameter_area/(2*spacing),\
+								fill=False, edgecolor=color, linewidth=1)
+		axs[0].add_patch(circ)
+	
+	#	axs[0].text(minc, minr-2, round(region.area_filled,2), color=color)
+	
+	for region in elongated_droplets:
+		minr, minc, maxr, maxc = region.bbox
+	
+		color = 'green'
+		rect = mpatches.Rectangle((minc-1, minr-1), maxc - minc, maxr - minr,
+									  fill=False, edgecolor=color, linewidth=1)
+		axs[0].add_patch(rect)
+		
+	#	axs[0].text(minc, minr-2, round(region.area_filled,2), color=color)
+	
+
+	print("Average droplet density:",\
+		  str((len(elongated_droplets) + len(round_droplets))* 10**6 / \
+		  	  (img_gray.shape[0]*img_gray.shape[1]*spacing**2)),\
+		  "droplets / mm^2 (ignoring focal plane depth)")
+
+	sizes = []
+	for drop in round_droplets:
+		sizes.append(drop.equivalent_diameter_area)
+	
+	
+	### Show the picture
+	
+	
+	axs[0].set_axis_off()
+	
+	axs[1].hist(sizes, bins=50, density=True)
+	axs[1].set_ylabel("Frequency")
+	axs[1].set_xlabel("Diameter ($\\mathrm{\\mu m}$)")
+	
+	plt.tight_layout()
+	prefix = name.split('.')[0]
+	plt.title("\\#".join(prefix.split('#')))
+	if save:
+		plt.savefig("out/" + prefix + ".pdf", dpi=600)
+	else:
+		plt.show()
+	
+
+
+
 ### Set the picture parameters
 
-datapath = '.'
+save = False
+
+if len(sys.argv) != 2:
+	print("Use a proper argument")
+else:
+	if sys.argv[1] == "all":
+		for file in os.listdir():
+			if file.split('.')[-1] in ['tif', 'png']:
+				print("Processing", file)
+				make_figure(file, file.split('.')[0].split('-')[-1], save)
+	else:
+		make_figure(sys.argv[1], sys.argv[1].split('.')[0].split('-')[-1], save)
+		
+		
+
+
 #prefix = '20251201-7-40x' # Uncrossed, hard
 #prefix = '20251201-3-20x' # Easy
 #prefix = '20251126-3-1-10x' # Shape change
-prefix = '20251205-5-20x' # Small shape change, uncrossed
+#prefix = '20251205-5-20x' # Small shape change, uncrossed
 #prefix = '20251126-1-1-5x' # Many drops. To be clear, 5x is a guess
-
-# Assume the pixel spacing is isotropic
-# The spacing is in um/px
-match prefix.split('-')[-1]:
-	case '5x':
-		spacing = 1/2.03
-	case '10x':
-		spacing = 1/4.09
-	case '20x':
-		spacing = 1/8.17
-	case '40x':
-		spacing = 1/16.56
-	case _:
-		spacing = 1
-		print("WARNING: No magnification found, guessing pixel size")
-	
-
-
-mpl.style.use("classic")
-plt.rcParams.update({
-    "text.usetex": True,
-    "font.family": "serif"
-})
-### Load and threshold the picture
-
-img_original = ski.io.imread(os.path.join(datapath, prefix + '.tif'))
-img_gray = ski.color.rgb2gray(ski.util.img_as_int(img_original))
-
-print("Loaded image")
-
-threshold = ski.filters.threshold_yen(img_gray)
-img_thresh = img_gray >= threshold
-print("Thresholded image")
-
-
-
-### Identify the droplets
-
-# Label elements on the picture
-black = ski.util.dtype_limits(img_thresh)[0]
-label_image = label(img_thresh, background=black)
-
-fig, axs = plt.subplots(ncols=1, nrows=2, figsize=(12, 12), height_ratios=[3,1])
-axs[0].imshow(img_original)
-
-round_droplets = []
-elongated_droplets = []
-
-# Analyze each droplet
-for region in regionprops(label_image, intensity_image=img_gray, spacing=(spacing,spacing)):
-	# Skip regions smaller than 50 square pixels
-	if region.area < 50 * spacing**2:
-		continue
-	
-	# Skip regions smaller than 2 um^2
-	if region.area < 2:
-		continue
-
-	# Skip regions close to the edge of the image
-	if region.bbox[0] < 10 or\
-	   region.bbox[1] < 10 or\
-	   region.bbox[2] > img_thresh.shape[0] - 10 or\
-	   region.bbox[3] > img_thresh.shape[1] - 10:
-	   continue
-
-#	print(round(region.area_filled,1), '\t', round(region.eccentricity,2))
-
-	if region.eccentricity > 0.7:
-		elongated_droplets.append(region)
-	else:
-		round_droplets.append(region)
-#		print('\t', round(region.equivalent_diameter_area,3))
-
-elongated_droplets, new_round = aux_functions.merge_nearby_regions(elongated_droplets)
-round_droplets += new_round
-
-### Display the droplet outlines
-
-for region in round_droplets:
-	minr, minc, maxr, maxc = region.bbox
-	color = 'red'
-	
-	circ = mpatches.Circle((region.centroid[1]/spacing, region.centroid[0]/spacing),\
-							radius=region.equivalent_diameter_area/(2*spacing),\
-							fill=False, edgecolor=color, linewidth=1)
-	axs[0].add_patch(circ)
-
-#	axs[0].text(minc, minr-2, round(region.area_filled,2), color=color)
-
-for region in elongated_droplets:
-	minr, minc, maxr, maxc = region.bbox
-
-	color = 'green'
-	rect = mpatches.Rectangle((minc-1, minr-1), maxc - minc, maxr - minr,
-								  fill=False, edgecolor=color, linewidth=1)
-	axs[0].add_patch(rect)
-	
-#	axs[0].text(minc, minr-2, round(region.area_filled,2), color=color)
-
-
-sizes = []
-for drop in round_droplets:
-	sizes.append(drop.equivalent_diameter_area)
-
-
-### Show the picture
-
-
-axs[0].set_axis_off()
-axs[1].hist(sizes, bins=50, density=True)
-axs[1].set_ylabel("Frequency")
-axs[1].set_xlabel("Diameter ($\\mathrm{\\mu m}$)")
-plt.tight_layout()
-plt.show()
 
 
